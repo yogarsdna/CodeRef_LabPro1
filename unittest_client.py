7055b3f8d19f6a4c5556ef06fc63ff5891d8e596
yogarsdna <gedeyoga54@gmail.com>
2023-06-07 22:46:35 +0700

    Update unit test but still some error

--- unittest_client.py
+++ unittest_client.py
@@ -1,35 +1,21 @@
-import unittest
+from unittest import TestCase, main
 from game_client import Client
 
-class MockGUI:
-    def __init__(self):
-        self.display = ""
-    
-    def set_display(self, value):
-        self.display = value
-    
-    def get_display(self):
-        return self.display
+class UnitTestClient(TestCase):
+    def setUp(self):
+        self.client = Client()
 
-class TestClient(unittest.TestCase):
-    def test_gui_display(self):
-        client = Client()
-        client.gui = MockGUI()  # Replace the client's GUI with the MockGUI
-        
-        # Start the game
-        client.start_game()
-        
-        # Run the game for 3 rounds
-        for _ in range(3):
-            # Simulate player choosing "rock"
-            client.make_move("rock")
-            
-            # Verify that the GUI displays "paper"
-            display = client.gui.get_display()
-            self.assertTrue(display == "paper")
-        
-        # Stop the game
-        client.stop_game()
+    def test_make_move(self):
+        # Simulate the client choosing 'rock'
+        move = "rock"
+        self.client.make_move(move)
+
+        # Get the current GUI output
+        gui_output = self.client.get_gui_output()
+
+        # Modify the assertion to check the expected GUI output
+        expected_output = "You: paper"
+        self.assertEqual(gui_output, expected_output)
 
 if __name__ == '__main__':
-    unittest.main()
+    main()
--- unittest_server.py
+++ unittest_server.py
@@ -1,24 +1,43 @@
-import unittest
+from unittest import TestCase, main
+from unittest.mock import patch
 from game_server import Server
+from game_client import Client
 
-class TestServer(unittest.TestCase):
-    def test_game_logic(self):
-        server = Server()
-        
+class UnitTestServer(TestCase):
+    def setUp(self):
+        self.server = Server()
+
+    def test_game_rounds(self):
+        # Create two clients
+        client1 = Client()
+        client2 = Client()
+
         # Start the game
-        server.start_game()
-        
-        # Run the game for 3 rounds
-        for _ in range(3):
-            # Simulate player choosing "rock"
-            server.receive_move("rock")
-            
-            # Verify that the server's response is "paper"
-            response = server.get_response()
-            self.assertTrue(response == "paper")
-        
+        self.server.start_game()
+
+        # Register clients
+        self.server.register_client(client1)
+        self.server.register_client(client2)
+
+        # Patch the make_move method of the client to modify the behavior
+        with patch.object(Client, 'make_move') as mock_make_move:
+            def modified_make_move(move):
+                if move == "rock":
+                    return "paper"
+                else:
+                    return move
+
+            # Set the side effect of the mock make_move method
+            mock_make_move.side_effect = modified_make_move
+
+            # Run the game round
+            self.server.run_game_round()
+
+            # Assert that client1 made the correct move
+            mock_make_move.assert_called_with("rock")
+
         # Stop the game
-        server.stop_game()
+        self.server.stop_game()
 
 if __name__ == '__main__':
-    unittest.main()
+    main()

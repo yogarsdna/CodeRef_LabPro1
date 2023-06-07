64f050356f5abbc30744e186ef5c7601d4c9ce6d
yogarsdna <gedeyoga54@gmail.com>
2023-06-08 01:03:14 +0700

    Ad more unit_testing

--- unit_testing.py
+++ unit_testing.py
@@ -1,53 +1,68 @@
 import unittest
 
-def pickRockPaperScissor(picked, output):
+def pick_rps(picked, output):
     if output == picked :
         return True
+    
     else :
         return False
     
-def yourTurnButton(your_turn, button_enable):
-    if your_turn == True and button_enable == True:
+def turn_button(your_turn, button):
+    if your_turn == True and button == True:
         return True
+    
+    elif your_turn == False and button == False:
+        return True
+    
     else :
         return False
     
-def greetingString(state, client1_name, client2_name, greetings):
+def welcome_message(state, client1_name, client2_name, greetings):
     if state == "client 1" and greetings == "Hello " + client1_name:
         return True
+    
     elif state == "client 2" and greetings == "Hello " + client2_name:
         return True
+    
     else : return False        
         
 
-class UnitTest (unittest.TestCase):
-    def test_pickedChoice_rockOutputPaper_falseReturn(self):
-        result = pickRockPaperScissor("rock", "paper")
+class UnitTest(unittest.TestCase):
+    def test_pick_rps_rock_paper(self):
+        result = pick_rps("rock", "paper")
         self.assertFalse(result)
 
-    def test_pickedChoice_roOutputPaper_falseReturn(self):
-        result = pickRockPaperScissor("scissor", "rock")
+    def test_pick_rps_scissor_rock(self):
+        result = pick_rps("scissor", "rock")
         self.assertFalse(result)
 
-    def test_pickedChoice_rockOutputRock_trueReturn(self):
-        result = pickRockPaperScissor("rock", "rock")
+    def test_pick_rps_rock_rock(self):
+        result = pick_rps("rock", "rock")
         self.assertTrue(result)
 
-    def test_yourTurn_rockOutputRock_falseReturn(self):
-        result = yourTurnButton(True, False)
+    def test_turn_button_your_disable(self):
+        result = turn_button(True, False)
         self.assertFalse(result)
 
-    def test_yourTurn_OutputRock_falseReturn(self):
-        result = yourTurnButton(False, True)
+    def test_turn_button_not_enable(self):
+        result = turn_button(False, True)
         self.assertFalse(result)
 
-    def test_yourTurn_rsOutputRock_falseReturn(self):
-        result = yourTurnButton(True, True)
+    def test_turn_button_your_enable(self):
+        result = turn_button(True, True)
         self.assertTrue(result)
 
-    def test_Turn_rsOutputRock_falseReturn(self):
-        result = greetingString("client 1", "John", "Zulu", "Hello Zulu")
+    def test_welcome_message_client1_client2(self):
+        result = welcome_message("client 1", "Yog", "Zulu", "Hello Zulu")
         self.assertFalse(result)
+
+    def test_welcome_message_client2_client1(self):
+        result = welcome_message("client 2", "Yog", "Zulu", "Hello Yog")
+        self.assertFalse(result)
+
+    def test_welcome_message_client1_client1(self):
+        result = welcome_message("client 1", "Yog", "Zulu", "Hello Yog")
+        self.assertTrue(result)
 
 if __name__ == '__main__':
     unittest.main()

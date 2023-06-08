f0295a7bee4a72388401967bc3f43b4d15ea713e
yogarsdna <gedeyoga54@gmail.com>
2023-06-08 19:42:06 +0700

    Update unit_testing.py

--- unit_testing.py
+++ unit_testing.py
@@ -17,7 +17,7 @@ def turn_button(your_turn, button):
     else :
         return False
     
-def welcome_message(state, client1_name, client2_name, greetings):
+def client_name(state, client1_name, client2_name, greetings):
     if state == "client 1" and greetings == "Hello " + client1_name:
         return True
     
@@ -52,16 +52,16 @@ class UnitTest(unittest.TestCase):
         result = turn_button(True, True)
         self.assertTrue(result)
 
-    def test_welcome_message_client1_client2(self):
-        result = welcome_message("client 1", "Yog", "Zulu", "Hello Zulu")
+    def test_client_name_client1_client2(self):
+        result = client_name("client 1", "Yog", "Zulu", "Name: Zulu")
         self.assertFalse(result)
 
-    def test_welcome_message_client2_client1(self):
-        result = welcome_message("client 2", "Yog", "Zulu", "Hello Yog")
+    def test_client_name_client2_client1(self):
+        result = client_name("client 2", "Yog", "Zulu", "Name: Yog")
         self.assertFalse(result)
 
-    def test_welcome_message_client1_client1(self):
-        result = welcome_message("client 1", "Yog", "Zulu", "Hello Yog")
+    def test_client_name_client1_client1(self):
+        result = client_name("client 1", "Yog", "Zulu", "Name: Yog")
         self.assertTrue(result)
 
 if __name__ == '__main__':
